4460f3205cc7458f381d8b73712a438ef47c3b28
Anigko <anigko@yandex.ru>
2022-03-27 18:22:03 +0300

    Initial commit

---
+++ src/main/java/ClientSocketApp.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientSocketApp {
+
+    public static void main(String[] args) {
+
+        try (Socket clientSocket = new Socket("localhost", 8089);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        ) {
+             out.println("Test");
+
+             String response = in.readLine();
+
+             System.out.println(response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
